b59df484b9347bc2894b6cbf90609fca7b5c52bc
Antonela <gemory.mei@gmail.com>
2023-04-17 09:50:15 +0000

    Added fixture, made FoodController return all items

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Food;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class FoodController extends AbstractController
 {
     #[Route('/food')]
-    public function indexAction(): JsonResponse
+    public function indexAction(EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->json([]);
+        $foodRepository = $entityManager->getRepository(Food::class);
+        return $this->json($foodRepository->findAll());
     }
 }
